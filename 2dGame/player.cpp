76b246edc2e50f41ba1ea90b46798cccc912d24e
sonya <sonya@SONYS>
2025-01-23 13:13:38 +0300

    исправление повторного выстрела

--- 2dGame/player.cpp
+++ 2dGame/player.cpp
@@ -3,20 +3,21 @@
 Player::Player() : ships_left(MAX_SHIPS), grid(GRID_SIZE, vector<int>(GRID_SIZE, 0)) {}
 
 bool Player::attack(int x, int y) {
+    if (grid[x][y] == 2 || grid[x][y] == -1) {
+        cout << "You have already attacked this position. Try again.\n";
+        return false;
+    }
+
     if (grid[x][y] == 1) {
         grid[x][y] = 2;  // Попадание
         ships_left--;
         return true;
     }
-    else if (grid[x][y] == 0) {
-        grid[x][y] = -1; // Промах
-        return false;
-    }
-    else {
-        cout << "You have already attacked this position. Try again.\n";
-        return false;
-    }
+
+    grid[x][y] = -1; // Промах
+    return false;
 }
+
 
 
 int Player::getShipsLeft() const {
@@ -49,7 +50,21 @@ void Player::saveGridToFile(const string& filename) {
 
     for (const auto& row : grid) {
         for (int cell : row) {
-            file << cell << " ";
+            if (cell == 2) {
+                file << "2 ";  // Попадание
+            }
+            else if (cell == -1) {
+                file << "-1 "; // Промах
+            }
+            else if (cell == 0) {
+                file << "0 ";  // Пустое поле
+            }
+            else if (cell == 1) {
+                file << "1 ";  // Корабль
+            }
+            else {
+                file << "X ";  // Неизвестное значение (на всякий случай)
+            }
         }
         file << "\n";
     }
@@ -57,6 +72,7 @@ void Player::saveGridToFile(const string& filename) {
     file.close();
     cout << "Grid successfully saved to " << filename << "\n";
 }
+
 
 
 void Player::loadGridFromFile(const string& filename) {
